c83a07cab52183797fe3b5fcc2a68e01a8f2011c
CooklePlex <cookleplex51760498+cookleplex@users.noreply.github.com>
2020-03-26 18:20:22 -0700

    Fixed glitch while tping & unsneaking Added particle while tping Added auto save for portals for in the event of a server crash the portals are saved
    Redid file system so that old portals are removed. Made a new class called PortalConfig for this.

--- src/main/java/me/cookle/portalCore/PortalListener.java
+++ src/main/java/me/cookle/portalCore/PortalListener.java
@@ -1,29 +1,22 @@
 package me.cookle.portalCore;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.World;
-import org.bukkit.block.Beacon;
-import org.bukkit.block.Block;
+import org.bukkit.*;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.util.BoundingBox;
 import org.jetbrains.annotations.NotNull;
 
 public class PortalListener implements Listener {
-    private Plugin plugin;
+    private PortalConfig config;
 
-    PortalListener(Main main) {
-        plugin = main;
+    PortalListener(PortalConfig portalConfig) {
+        config = portalConfig;
     }
 
     @EventHandler
@@ -32,79 +25,45 @@ public class PortalListener implements Listener {
         Player player = event.getPlayer();
         if (player.isSneaking()) return;
 
-        Block block = player.getLocation().subtract(0.0, 1.0, 0.0).getBlock();
-        if (block.getType() != Material.BEACON) return;
-
-        Beacon beacon = (Beacon) block.getState();
-        if (beacon.getTier() == 0) return;
-
-        Location originLocation = block.getLocation();
-        List<String> thisID = Main.getPortalID(originLocation);
-        if (thisID == null) return;
+        Location origin = player.getLocation().subtract(0, 1, 0);
+        Location blockOffset = origin.clone().subtract(origin.getBlockX(), origin.getBlockY(), origin.getBlockZ());
+        if(config.checkBeacon(origin)) return;
 
         World world = player.getWorld();
+        if(config.setPortalLink(origin)) world.spawnParticle(Particle.LAVA, player.getLocation(), 5);
 
-        if (!plugin.getConfig().contains(thisID.toString())) {
-            plugin.getConfig().set(thisID.toString(), Main.getStringFromLocation(originLocation));
-            world.playSound(originLocation, Sound.ITEM_FLINTANDSTEEL_USE, 16, 1);
-        }
+        Location destination = config.getPortalLink(origin);
+        if(destination == null) return;
 
-        for (int t = 1; t < 10; t++) {
-            List<String> otherID = Main.getPortalID(originLocation.clone().add(0.0, t, 0.0));
-            if (otherID == null) continue;
+        Location locationOffset = destination.subtract(origin).add(blockOffset);
 
-            String strLoc = plugin.getConfig().getString(otherID.toString());
-            if (strLoc == null) continue;
+        Collection<Mob> leashedEntities = world.getNearbyEntities(
+                BoundingBox.of(
+                        origin, 5,5,5
+                ))
+                .parallelStream().filter(entity -> entity instanceof Mob)
+                .map(entity -> (Mob)entity)
+                .filter(mob -> mob.isLeashed() && mob.getLeashHolder().equals(player))
+                .collect(Collectors.toSet());
 
-            Location destinationLocation = Main.getLocationFromString(strLoc);
-            Block destinationBlock = destinationLocation.getBlock();
+        world.playSound(origin, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
+        world.spawnParticle(Particle.FLASH, origin, 10, 0.2, 2, 0.2);
 
-            if (destinationBlock.getType() != Material.BEACON) {
-                plugin.getConfig().set(otherID.toString(), null);
-                continue;
-            }
+        Location playerDestination = player.getLocation();
+        playerDestination.add(locationOffset);
+        player.teleport(playerDestination);
+        world.playSound(playerDestination, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
+        world.spawnParticle(Particle.FLASH, playerDestination, 20, 0.2, 1, 0.2);
 
-            Beacon destinationBeacon = (Beacon)destinationBlock.getState();
-            if (destinationBeacon.getTier() == 0) continue;
+        leashedEntities.forEach(mob -> {
+            Location mobLocation = mob.getLocation();
+            Location mobDestination = mobLocation.add(locationOffset);
+            mob.teleport(mobDestination);
+            world.spawnParticle(Particle.FLASH, mobDestination, 20, 0.2, 1, 0.2);
+            mob.setLeashHolder(player);
+        });
 
-            List<String> destinationID = Main.getPortalID(destinationLocation);
-            if (destinationID == null) {
-                plugin.getConfig().set(otherID.toString(), null);
-                continue;
-            }
-
-            if (!destinationID.equals(otherID)) {
-                plugin.getConfig().set(otherID.toString(), null);
-                plugin.getConfig().set(destinationID.toString(), Main.getStringFromLocation(destinationLocation));
-                continue;
-            }
-
-            Location locationOffset = destinationLocation.subtract(originLocation);
-
-            Collection<Mob> leashedEntities = world.getNearbyEntities(
-                    BoundingBox.of(
-                            originLocation, 3,3,3
-                    ))
-                    .parallelStream().filter(entity -> entity instanceof Mob)
-                    .map(entity -> (Mob)entity)
-                    .filter(mob-> mob.isLeashed() && mob.getLeashHolder().equals(player))
-                    .collect(Collectors.toSet());
-
-            world.playSound(originLocation, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
-
-            Location playerDestination = player.getLocation();
-            playerDestination.add(locationOffset);
-            player.teleport(playerDestination);
-            world.playSound(playerDestination, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
-
-            leashedEntities.forEach(mob -> {
-                Location mobLocation = mob.getLocation();
-                Location mobDestination = mobLocation.add(locationOffset);
-                mob.teleport(mobDestination);
-                mob.setLeashHolder(player);
-            });
-            break;
-        }
+        player.setSneaking(false);
     }
 }
 
