4ef61805f52cd0de425c38aae9de66aee405d130
Cookleplex <51760498+cookleplex@users.noreply.github.com>
2020-04-18 19:14:07 -0700

    Fixed world to world teleport

--- src/main/java/me/cookle/portalCore/PortalListener.java
+++ src/main/java/me/cookle/portalCore/PortalListener.java
@@ -26,18 +26,15 @@ public class PortalListener implements Listener {
         if (player.isSneaking()) return;
 
         Location origin = player.getLocation().subtract(0, 1, 0);
-        Location blockOffset = origin.clone().subtract(origin.getBlockX(), origin.getBlockY(), origin.getBlockZ());
         if(config.checkBeacon(origin)) return;
 
-        World world = player.getWorld();
-        if(config.setPortalLink(origin)) world.spawnParticle(Particle.LAVA, player.getLocation(), 5);
+        World originWorld = player.getWorld();
+        if(config.setPortalLink(origin)) originWorld.spawnParticle(Particle.LAVA, player.getLocation(), 5);
 
         Location destination = config.getPortalLink(origin);
         if(destination == null) return;
 
-        Location locationOffset = destination.subtract(origin).add(blockOffset);
-
-        Collection<Mob> leashedEntities = world.getNearbyEntities(
+        Collection<Mob> leashedEntities = originWorld.getNearbyEntities(
                 BoundingBox.of(
                         origin, 5,5,5
                 ))
@@ -46,20 +43,29 @@ public class PortalListener implements Listener {
                 .filter(mob -> mob.isLeashed() && mob.getLeashHolder().equals(player))
                 .collect(Collectors.toSet());
 
-        world.playSound(origin, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
-        world.spawnParticle(Particle.FLASH, origin, 10, 0.2, 2, 0.2);
+        originWorld.playSound(origin, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
+        originWorld.spawnParticle(Particle.FLASH, origin, 10, 0.2, 2, 0.2);
+
+        World destinationWorld = destination.getWorld();
+        if(destinationWorld == null) return;
+
+        origin.setWorld(destinationWorld);
+        Location blockOffset = origin.clone().subtract(origin.getBlockX(), origin.getBlockY(), origin.getBlockZ());
+
+        Location locationOffset = destination.subtract(origin).add(blockOffset);
 
         Location playerDestination = player.getLocation();
+        playerDestination.setWorld(destinationWorld);
         playerDestination.add(locationOffset);
         player.teleport(playerDestination);
-        world.playSound(playerDestination, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
-        world.spawnParticle(Particle.FLASH, playerDestination, 20, 0.2, 1, 0.2);
+        destinationWorld.playSound(playerDestination, Sound.ENTITY_ENDERMAN_TELEPORT, 16, 16);
+        destinationWorld.spawnParticle(Particle.FLASH, playerDestination, 20, 0.2, 1, 0.2);
 
         leashedEntities.forEach(mob -> {
             Location mobLocation = mob.getLocation();
             Location mobDestination = mobLocation.add(locationOffset);
             mob.teleport(mobDestination);
-            world.spawnParticle(Particle.FLASH, mobDestination, 20, 0.2, 1, 0.2);
+            destinationWorld.spawnParticle(Particle.FLASH, mobDestination, 20, 0.2, 1, 0.2);
             mob.setLeashHolder(player);
         });
 
